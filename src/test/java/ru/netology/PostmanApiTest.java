9dcb36a261c59b3dc776c10f23221f76b5f0677a
Alina <nyrete@gmail.com>
2021-03-16 16:13:50 +0500

    добавление теста

---
+++ src/test/java/ru/netology/PostmanApiTest.java
@@ -0,0 +1,25 @@
+package ru.netology;
+
+import io.restassured.specification.Argument;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+
+class PostmanApiTest {
+    @Test
+    void shouldReturnSomeData() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some value"))
+        ;
+    }
+}
